1bcdd481ec5682687d5ec61ef0f91265137508e9
reen967 <50892512+reen967@users.noreply.github.com>
2024-07-15 13:15:54 +0100

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,7 +7,8 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'Workforce Integration Solutions',
-  tagline: 'Reinventing the way job seekers and employers connect.',
+  tagline1: 'Reinventing the way job seekers and employers connect.',
+  tagline2: 'I am an',
   url: 'https://your-docusaurus-test-site.com',
   baseUrl: '/',
   onBrokenLinks: 'throw',
